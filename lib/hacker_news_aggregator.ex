f71e347aaa9cd30eb4aa0d66e790e5bc3db505c1
Ivan Ernandorena <ivan.ernandorena@gmail.com>
2021-07-10 15:07:06 -0300

    First commmit - mix new

---
+++ lib/hacker_news_aggregator.ex
@@ -0,0 +1,18 @@
+defmodule HackerNewsAggregator do
+  @moduledoc """
+  Documentation for `HackerNewsAggregator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HackerNewsAggregator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
